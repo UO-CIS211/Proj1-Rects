dfed8c99c404871b8a5f52f052d254ed3eeb2ae3
Michal Young <michal.young@gmail.com>
2018-03-22 18:47:34 -0700

    Spring 2018 version

--- intersect.py
+++ intersect.py
@@ -2,9 +2,9 @@
 Draw rectangles
 (driver program for rects.py)
 """
-import graphics
+import graphics.graphics
 import argparse
-import rects
+import rect
 
 import logging
 logging.basicConfig()
@@ -17,7 +17,8 @@ CANVAS_HEIGHT = 800
 
 def cli():
     """Command line arguments for drawing two rectangles"""
-    parser = argparse.ArgumentParser("Draw rectangles")
+    parser = argparse.ArgumentParser("Intersect rectangles")
+    parser.add_argument("--display", "-d",  action="store_true")
     parser.add_argument(
         "ll_x1", type=int,
         help="X coordinate (in pixels) of lower left corner, first rect")
@@ -49,9 +50,9 @@ def cli():
 def draw_rect(canvas, rect, color="black"):
     """Draw the rectangle on the canvas"""
     log.debug("Drawing {}".format(repr(rect)))
-    ll_view = graphics.Point(rect.ll.x, rect.ll.y)
-    ur_view = graphics.Point(rect.ur.x, rect.ur.y)
-    view = graphics.Rectangle(ll_view, ur_view)
+    ll_view = graphics.graphics.Point(rect.ll.x, rect.ll.y)
+    ur_view = graphics.graphics.Point(rect.ur.x, rect.ur.y)
+    view = graphics.graphics.Rectangle(ll_view, ur_view)
     view.setFill(color)
     view.draw(canvas)
 
@@ -59,23 +60,25 @@ def draw_rect(canvas, rect, color="black"):
 def main():
     """Draw two rectangles on a canvas"""
     args = cli()
-    canvas = graphics.GraphWin("Rects", CANVAS_WIDTH, CANVAS_HEIGHT)
-    r1 = rects.Rect(
-        rects.Point(args.ll_x1, args.ll_y1),
-        rects.Point(args.ur_x1, args.ur_y1))
-    draw_rect(canvas, r1, color="red")
+    r1 = rect.Rect(
+        rect.Point(args.ll_x1, args.ll_y1),
+        rect.Point(args.ur_x1, args.ur_y1))
     log.info("Rectangle {}".format(r1))
-    r2 = rects.Rect(
-        rects.Point(args.ll_x2, args.ll_y2),
-        rects.Point(args.ur_x2, args.ur_y2))
-    draw_rect(canvas, r2, color="blue")
+    r2 = rect.Rect(
+        rect.Point(args.ll_x2, args.ll_y2),
+        rect.Point(args.ur_x2, args.ur_y2))
     log.info("Rectangle {}".format(r2))
-    canvas.getMouse()
+    if args.display:
+        canvas = graphics.graphics.GraphWin("Rects", CANVAS_WIDTH, CANVAS_HEIGHT)
+        draw_rect(canvas, r1, color="red")
+        draw_rect(canvas, r2, color="blue")
+        input("Press enter to intersect")
     r3 = r1.intersect(r2)
     print("Intersection: {}".format(r3))
-    draw_rect(canvas, r3, color="purple")
-    canvas.getMouse()   # Pause for user to click on screen
-    canvas.close()
+    if args.display:
+        draw_rect(canvas, r3, color="purple")
+        input("Press enter to finish")
+        canvas.close()
 
 
 if __name__ == "__main__":
--- rect.py
+++ rect.py
@@ -12,23 +12,17 @@ CIS 211 Project 1
 Author:  FIXME (your name here)
 UO email: FIXME
 """
-
-import numbers
-
 import logging
 logging.basicConfig()
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-# To turn on debugging output, change the above to
-# log.setLevel(logging.DEBUG)
+log.setLevel(logging.DEBUG)
 
 
 class Point(object):
     """A point is an ordered pair, (x,y)"""
 
-    def __init__(self, x, y):
-        assert isinstance(x, numbers.Number)
-        assert isinstance(y, numbers.Number)
+    def __init__(self, x: int, y: int):
+        assert isinstance(x,int) and isinstance(y, int)
         self.x = x
         self.y = y
         log.debug("Created Point {}".format(repr(self)))
@@ -39,8 +33,7 @@ class Point(object):
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
 
-    def __eq__(self, other):
-        assert(isinstance(other, Point))
+    def __eq__(self, other: 'Point'):
         return self.x == other.x and self.y == other.y
 
 
@@ -60,25 +53,36 @@ class Rect(object):
         log.debug("Created rect {}".format(repr(self)))
 
     def __repr__(self):
-        return "Rect({},{})".format(self.ll, self.ur)
+        return "Rect({},{})".format(repr(self.ll), repr(self.ur))
 
     def __str__(self):
         return "Rect({},{})".format(self.ll, self.ur)
 
-    def __eq__(self, other):
-        assert isinstance(other, Rect)
+    def __eq__(self, other: 'Rect'):
         return self.ll == other.ll and self.ur == other.ur
 
     def overlaps(self, other):
-        """r1.overlaps(r2) if there is an area of positive
-        size within r1 and also within r2.  "Of positive size"
-        means that touching at a corner or along an edge is
-        not enough ... the area of overlap must be positive.
-        """
-        # FIXME  return the correct boolean value
-        return False
+        if self.ur.x <= other.ll.x:
+            # self is to the left of other
+            return False
+        if self.ur.y <= other.ll.y:
+            # self is below other
+            return False
+        if self.ll.x >= other.ur.x:
+            # self is to the right of other
+            return False
+        if self.ll.y >= other.ur.y:
+            # self is above other
+            return False
+        return True
 
-    def intersect(self, other):
+    def intersect(self, other: 'Rect'):
         """Region of overlap, or (0,0),(0,0) if none"""
-        # FIXME  calculate the region of overlap.
-        return Rect(Point(0, 0), Point(0, 0))
+        if self.overlaps(other):
+            ll = Point(max(self.ll.x, other.ll.x),
+                       max(self.ll.y, other.ll.y))
+            ur = Point(min(self.ur.x, other.ur.x),
+                       min(self.ur.y, other.ur.y))
+            return Rect(ll, ur)
+        else:
+            return Rect(Point(0, 0), Point(0, 0))
